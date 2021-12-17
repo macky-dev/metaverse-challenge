01aefbfcb716fa5d62fc198695e13f239bdfb54c
Mark Kevin Nava Martinez <macky@Marks-MacBook-Pro.local>
2021-12-17 21:11:36 +0800

    chat functionality done

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import Head from "next/head";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
 import Login from "../components/Login";
+import Messages from "../components/Messages";
 
 const Home = () => {
-  const { isAuthenticated, logout } = useMoralis();
+  const { isAuthenticated } = useMoralis();
 
   if (!isAuthenticated) {
     return <Login />;
   }
 
   return (
-    <div className="">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to the APP</h1>
-      <button onClick={logout}>Logout</button>
+      <div className="max-w-screen-lg mx-auto">
+        <Header />
+        <Messages />
+      </div>
     </div>
   );
 };
